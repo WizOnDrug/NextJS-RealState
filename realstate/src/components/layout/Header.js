b9832a6b0d73b294e67fe92ff4c3bd4f445f1e40
Wiz <im.not.ali@outlook.com>
2023-12-29 03:12:03 +0330

    Add Header and Footer and syles

--- realstate/src/components/layout/Header.js
+++ realstate/src/components/layout/Header.js
@@ -4,10 +4,9 @@ import Link from "next/link";
 import { FiLogIn } from "react-icons/fi";
 import { FaUserAlt } from "react-icons/fa";
 import styles from "@/layout/Header.module.css";
-import { useSession } from "next-auth/react";
+
 
 const Header = () => {
-  const { data } = useSession();
   return (
     <header className={styles.header}>
       <div>
@@ -20,20 +19,6 @@ const Header = () => {
           </li>
         </ul>
       </div>
-      {data ? (
-        <div className={styles.login}>
-          <Link href="/dashboard">
-            <FaUserAlt />
-          </Link>
-        </div>
-      ) : (
-        <div className={styles.login}>
-          <Link href="/signin">
-            <FiLogIn />
-            <span>ورود</span>
-          </Link>
-        </div>
-      )}
     </header>
   );
 };
