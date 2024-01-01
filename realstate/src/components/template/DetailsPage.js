e3b9af864e04d61a9fd692a83558843f002069e7
Wiz <im.not.ali@outlook.com>
2024-01-02 01:14:33 +0330

    Add Page for Details of advertising

---
+++ realstate/src/components/template/DetailsPage.js
@@ -0,0 +1,70 @@
+import { SiHomebridge } from "react-icons/si";
+import { AiOutlinePhone } from "react-icons/ai";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { BiCalendarCheck } from "react-icons/bi";
+import ShareButton from "./ShareButton";
+import Title from "@/module/Title";
+import ItemList from "@/module/ItemList";
+import { e2p, sp } from "@/utils/replaceNumber";
+import { icons } from "@/constans/icons";
+import { categories } from "@/constans/strings";
+import styles from "@/template/DetailsPage.module.css";
+
+const DetailsPage = ({
+  data: {
+    title,
+    location,
+    description,
+    amenities,
+    rules,
+    realState,
+    phone,
+    price,
+    category,
+    constructionDate,
+  },
+}) => {
+ 
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <h1>{title}</h1>
+        <span>
+          <HiOutlineLocationMarker />
+          {location}
+        </span>
+        <Title>توضیحات</Title>
+        <p>{description}</p>
+        <Title>امکانات رفاهی</Title>
+        <ItemList data={amenities} />
+        <Title>قوانین</Title>
+        <ItemList data={rules} />
+      </div>
+      <div className={styles.sidebar}>
+        <div className={styles.realState}>
+          <SiHomebridge />
+          <p>املاک {realState}</p>
+          <span>
+            <AiOutlinePhone />
+            {e2p(phone)}
+          </span>
+        </div>
+        <ShareButton />
+        <div className={styles.price}>
+          <p>
+            {icons[category]}
+            {categories[category]}
+          </p>
+          <p>{sp(price)} تومان</p>
+          <p>
+            <BiCalendarCheck />
+            {new Date(constructionDate).toLocaleDateString("fa-IR")}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailsPage;
