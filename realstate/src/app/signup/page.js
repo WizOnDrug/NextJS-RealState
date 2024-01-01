3ceb38894b0186ff712d6bcc7ac67f5b5b3d378b
Wiz <im.not.ali@outlook.com>
2024-01-01 20:11:05 +0330

    SignUp and Signin page and connect to db

---
+++ realstate/src/app/signup/page.js
@@ -0,0 +1,12 @@
+import SignupPage from "@/template/SignupPage";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+const Signup = async () => {
+  const session = await getServerSession(authOptions);
+  if (session) redirect("/");
+  return <SignupPage />;
+};
+
+export default Signup;
--- realstate/src/components/layout/Header.js
+++ realstate/src/components/layout/Header.js
@@ -1,12 +1,12 @@
 "use client";
-"use client";
 import Link from "next/link";
 import { FiLogIn } from "react-icons/fi";
 import { FaUserAlt } from "react-icons/fa";
 import styles from "@/layout/Header.module.css";
-
+import { useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data } = useSession();
   return (
     <header className={styles.header}>
       <div>
@@ -19,6 +19,20 @@ const Header = () => {
           </li>
         </ul>
       </div>
+      {data ? (
+        <div className={styles.login}>
+          <Link href="/dashboard">
+            <FaUserAlt />
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.login}>
+          <Link href="/signin">
+            <FiLogIn />
+            <span>ورود</span>
+          </Link>
+        </div>
+      )}
     </header>
   );
 };
